2acd343c2e38098a28aa9df1afd74526ca34936d
elisego <elise.godard@gmail.com>
2020-03-20 15:29:26 +0100

    resolution du 1 et 11 pour l'as

--- Play.java
+++ Play.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 class Play {
 
-    
+
     public static void main(String[] args) {
 
         System.out.println("Bienvenue au blackjack de la WCS");
@@ -12,15 +12,24 @@ class Play {
         int countDealer = 0;
         
         int card1 = takeCard();
+        card1 = choice(card1);
         System.out.println("Le joueur a pioché la carte : " + card1);
         int card2 = takeCard();
+        card2 = choice(card2);
         System.out.println("Le joueur a pioché la carte : " + card2);
         countJ1 = card1 + card2;
         System.out.println("Le joueur 1 a " + countJ1 + " points");
         int cardDealer1 = takeCard();
+        if (cardDealer1 == 1){
+            cardDealer1 = 11;
+        }
         System.out.println("Le croupier a pioché la carte : " + cardDealer1);
         int cardDealer2 = takeCard();
         System.out.println("Le croupier a pioché une carte face cachée ");
+        countDealer = cardDealer1 + cardDealer2;
+        if ( cardDealer2==1 && countDealer< 17){
+            cardDealer2=11;    
+        }
         countDealer = cardDealer1 + cardDealer2;
         while (countJ1 < 21) {
             Scanner sc = new Scanner(System.in);
@@ -29,6 +38,7 @@ class Play {
             if (reponse.equals("oui")) { 
                 System.out.println("le joueur a décidé de continuer");
                 int card = takeCard();
+                card =choice(card);
                 System.out.println("Le joueur a pioché la carte : " + card);
                 countJ1 = countJ1 + card;
                 System.out.println("le score de joueur 1 est : " + countJ1);
@@ -46,22 +56,33 @@ class Play {
             System.out.println("le dealer révèle la deuxième carte :" + cardDealer2);
             while (countDealer <= 16) {
                 int cardDealer = takeCard();
+                if (cardDealer ==1) {
+                    if ( countDealer + 11 >= 16 && countDealer + 11<21){
+                        cardDealer = 11;
+                    }else{
+                        cardDealer = 1;
+                    }
+                }
                 System.out.println("le dealer a pioché la carte :" + cardDealer);
-                countDealer = countDealer + cardDealer;   
+                countDealer = countDealer + cardDealer; 
+                System.out.println("le score du dealer est : " + countDealer);
             }
-            System.out.println("le score du dealer est : " + countDealer);
+            
+        
         }
         
         
         // TODO : vérifier qui a gagné
         if (countJ1 > 21) {
             System.out.println("le joueur 1 a perdu!");
+        } else if (countDealer > 21){
+            System.out.println("le croupier a perdu!");
         } else if (countJ1>countDealer) {
             System.out.println("le joueur 1 a gagné !");
         } else if (countJ1 == countDealer) {
-            System.out.println("égalité");
+            System.out.println("le joue est a égalité avec le croupier");
         } else {
-            System.out.println("le joueur 1 a perdu!");
+            System.out.println("le joueur 1 a perdu! et le croupier a gagné !! ");
         }
 
 
@@ -75,5 +96,24 @@ class Play {
         int card = 1 + rand.nextInt(10);
         return card;
     }
+
+    public static int choice(int card){
+        if (card == 1) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Voulez vous que l'As est une valeur de 1 ou 11 ?");
+            int choice = sc.nextInt();
+            if (choice == 1) { 
+                System.out.println("le joueur a décidé que l'As vaut 1");
+                card = 1;
+            }
+            else  {
+                System.out.println("le joueur a décidé que l'As vaut 11");
+                card = 11;
+            }
+            
+        }
+        return card;
+    }
+
 }
 
